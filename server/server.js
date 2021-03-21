e4965aec623944792ffee8949afe4bb68ec4a1a2
patricklapgar <patricklapgardeveloper@gmail.com>
2021-03-20 21:37:40 -0500

    First commit

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const SpotifyWebApi = require('spotify-web-api-node');
+
+const app = express();
+
+app.post('/login', (req, res) => {
+    const code = req.body.code;
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: 'http://localhost:3000',
+        clientId: '0cd8c9f36df54e72b16c5efae750c68a',
+        clientSecret: 'a848c31e680b4a67a66c266ceabb2402'
+    })
+
+    // Create authorization promise
+    spotifyApi.authorizationCodeGrant(code).then(data => {
+        res.json({
+            accessToken: data.body.access_token,
+            refreshToken: data.body.refresh_token,
+            expiresIn: data.body.expires_in
+        })
+    }).catch(() => {
+        res.sendStatus(400);
+    })
+})
